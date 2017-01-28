a05d3448023c93341a77d8a6d4db1532adff8d05
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-27 16:26:18 -0800

    yada-yada

--- provision.ps1
+++ provision.ps1
@@ -93,7 +93,9 @@ SCRIPT2
 
 Vagrant.configure("2") do |config|
   config.vm.box = "$vmname"
-config.vm.provision "shell", path: "disable_auto_proxy.ps1", privileged: true
+config.vm.provision privileged: true
+
+config.vm.provision :shell, :path => "disable_auto_proxy.ps1", :powershell_elevated_interactive => true
 
 config.vm.provider "virtualbox" do |v|
   v.memory = 4024
