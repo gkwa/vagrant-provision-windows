a9e65838c7775d92528235a4265307a9005ad1de
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-27 17:59:26 -0800

    yada-yada

--- provision.ps1
+++ provision.ps1
@@ -95,12 +95,46 @@ SCRIPT2
 echo 1 | Out-File -encoding 'ASCII' 'C:\Windows\Temp\out.txt'
 SCRIPT3
 
+`$script4 = <<'SCRIPT4'
+
+@"
+
+'http://www.htguk.com/disabling-internet-explorer/
+
+Option Explicit
+On Error Resume Next
+'Create a constant for the HKEY_CURRENT_USER object
+Const HKCU = &H80000001
+'Define variables
+Dim strComputer
+Dim strRegistryKey
+Dim objRegistry
+Dim strRegistryValue
+Dim binValue
+strComputer = "."
+strRegistryKey = "Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections"
+strRegistryValue = "DefaultConnectionSettings"
+'Connect to the Registry
+Set objRegistry = GetObject("winmgmts:\\" & strComputer & "\root\default:StdRegProv")
+'Retrieve the current settings.
+objRegistry.GetBinaryValue HKCU, strRegistryKey, strRegistryValue, binValue
+'Change the 'Automatically detect settings' box to unticked
+binValue(8) = 01
+'binValue(8) = 13 - Enable this line to check the box instead of uncheck
+'Save the changes
+objRegistry.SetBinaryValue HKCU, strRegistryKey, strRegistryValue, binValue
+
+"@ | Out-File -encoding 'ASCII' disable_auto_proxy.vbs
+
+SCRIPT4
+
 Vagrant.configure("2") do |config|
   config.vm.box = "$vmname"
 
 # config.vm.provision :shell, :path => "disable_auto_proxy.ps1"
 # config.vm.provision "not running" OR "not being run" powershell
-config.vm.provision "shell", inline: `$script3
+# config.vm.provision "shell", inline: `$script3
+config.vm.provision "shell", inline: `$script4
 
 config.vm.provider "virtualbox" do |v|
   v.memory = 4024
